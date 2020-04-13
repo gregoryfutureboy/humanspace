d08c914a881a1a4f135098e2ce4012056c1c18d8
Gregory Futureboy <31745194+gregoryfutureboy@users.noreply.github.com>
2020-04-12 22:30:36 -0400

    isop try 2

--- src/pages/peace.js
+++ src/pages/peace.js
@@ -1,81 +1,101 @@
 import React from 'react';
+import styled from '@emotion/styled';
+import { graphql, useStaticQuery } from 'gatsby';
+import { BLOCKS } from "@contentful/rich-text-types"
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 
 const Peace = () => {
+
+  const data = useStaticQuery(graphql`
+    query {
+      allContentfulWeeklyDoseArticleArticlePost2RichTextNode {
+        nodes {
+          json
+        }
+      }
+      allContentfulAsset(filter: { title: { eq: "Nefertiti" } }) {
+        nodes {
+          id
+          fixed {
+            src
+          }
+        }
+      }
+    }
+  `)
+
+  console.log(data)
+
+  const document =
+    data.allContentfulWeeklyDoseArticleArticlePost2RichTextNode.nodes[0].json
+
+  const options = {
+    renderNode: {
+      [BLOCKS.PARAGRAPH]: (node, children) => <Paragraphs>{children}</Paragraphs>,
+    },
+  }
+
   return (
-    <p>
-      Where have we been? Journeying through our minds in search of domination, war, destruction, control, superiority, greed and power.
-
-Fighting thoughts with thoughts.
-
-This was my prior path. It’s brutality has proven me to be an insurmountable mountain. I am brought to write that the Everest must be a simpler climb for the women & men of Silence. I can imagine them  moving towards their dream as I struggle to climb out of my bed with a smile.
-
-Now, I am brought back to myself. Facing the mirror, looking into my eyes, only I know what is happening inside. Or do I? If I don't understand the outside, nor the inside, what do I know?
-
-Over the last 3 years, and specifically over the last 12 months, I've felt an acceleration towards learning to surrender all of my thoughts, moods and inability to comprehend what is happening to the Higher Powers of Life.
-
-This call for inner & outter help has helped me so much. It brought me to experience many wonders and an indescribable support. I now believe that this help is Love sent from our Highest, Deepest core & remains accessible to all of Us at all time.
-
-I pray that You may find it as well. May this help bring Grace & Relief to all of Your dispairs.
-
-Being guided to welcome new thoughts, desired energies, outcomes, and letting go of the old (even if that means 1sec after one thought! :), as brought tremendous release into my Heart and Being. I am still far from a state of wellness but as I learn to picture myself experiencing well-being under the Radiant Sun, I pray that all of Us get to co-create a World into which We Live Together in inner & outer Harmony.
-
-From the bottom of my Heart, in these times of introspection, healing and mass awakenings, I wish everyone to feel the release that Breath, as well as the inner wonders of creation, can bring to You. 
-
-This search for Peace, a search for Freedom, Silence, Destiny, Visions, Insights, Actions, Compassion, Love & Clarity. An alchemical quest to transform feelings of fears & constrictions into Bliss Absolute; Satchidananda as the masters express it.
-
-May pure Gold, Inner Radiance and Expansion be bestowed on You.
-
-In this search for Peace, Life has also blessed me with the experience of the Divine Feminine. From progressively reconnecting to Mother Earth, to being embraced by Amma, to seeing my little Sister evolved into her magical Self as well as learning to make Love to an incredible Woman; I remain speechless & awed by the Power, Magic, Strength, Playfullness, Care, Compassion, Gentleness & Love of the Divine Feminine.
-
-This encounter of the Divine Feminine has also pushed me to reconsider and slowly start the healing process of my relationship with my mom. To say the least, it is one of my biggest challenges but I believe it is progressing.
-
-This said: Goddess Bless.
-
-The resurgence of the Women; the Priestesses, Mother Warriors of the Heart and Goddesses bring joy to my soul and this resurgence will help all of Us.
-
-May Ultimate Courage & Strength be givien to Us as We welcome the experience of Ultimate Balance, Love & Harmony, here on Planet Earth, & Beyond.
-
-Also, my prayers go to all my Brothers, from all Nations, religions, beliefs & cultures, to awaken, contemplate and connect with their Divine Feminine energies as well as exploring and connecting with the Mother & Goddess that gently watches over them. To learn to respect Life & be grateful might well be the key to a greater existence. In my journey, this exploration is surely becoming a vast ocean of Love into which I am slowly learning to bath into.
-
-For Our next Breath is in Her Hands.
-
-May We let go, in Peace, of the old Patriacharcal patterns, beliefs and way of LIfe that has oppressed us for so long.
-
-Please Mother, may all of Us be Healthy, Protected, Healed, Reassured, Calm, Loved & Well in this ascenscion process.
-
-Thank You.
-
-Thank you Mother. Thank you for the gift of Life, the Gift of Sight, the Gift of Touch, the Gift of  Hearing, Visualizing, Dreaming, Singing, Eating and Hugging.
-
-Thank you for our Hands, thank you for our Body and all of the Cells & natural wonders that live within ourselves to keep us alive without us even noticing and knowing about it!
- We are sorry.
-
-We are sorry for all of the pain, the destruction, the carelessness, and the hatred that lives in us.  We Love You.
-
-We Love You so much. I pray that we open our Hearts to send You into this Infinite well of Love that We have for You.  Forgive Us.
-
-Please forgive us for our ignorance as we are learning to Love, to Unite & Understand that this
-next Breath is truly, a true Blessing.
-
-May Her era begin,
-
-May Her Queendom Live,
-
-May We Remember,
-
-May Peace prevail on Earth and across all Dimensions of the Universe,
-
-With Love & Light,
-
-Team One.
-
-Namaste
-
-Gregory
-
-PS: I would like to Thank Paulo, thank you for planting the seeds of Transmutation into the collective and making them accessible through a fantastic story. I will never forget being captivated by your lines of Life flowing through the Alchemist.
-    </p>
+    <ArticleContainer>
+      <ArticleTitle>In Search of Peace</ArticleTitle>
+      <ArticleInformationContainer>
+        <ArticleInformation>
+          <p>The Rebirth — Weekly Dose — 001</p>
+        </ArticleInformation>
+        <ArticleInformation>
+          <p>April 12th, 2020</p>
+        </ArticleInformation>
+      </ArticleInformationContainer>
+      <ArticleImage src={data.allContentfulAsset.nodes[0].fixed.src} />
+      <Article>{documentToReactComponents(document, options)}</Article>
+    </ArticleContainer>
   )
 }
+
+const ArticleContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(12, 1fr);
+  grid-template-rows: repeat(8, 1fr) min-content;
+`
+
+const ArticleTitle = styled.h1`
+  grid-column: 5 / span 8;
+  grid-row: 6/7;
+  font-family: Arial;
+  font-weight: bold;
+  font-size: 80px;
+`
+
+const ArticleInformationContainer = styled.div`
+  grid-column: 7 / span 4;
+  grid-row: 7/8;
+  align-self: flex-end;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+`
+
+const ArticleInformation = styled.span`
+  margin: 0.3rem 0;
+  font-size: 20px;
+`
+
+const ArticleImage = styled.img`
+  grid-column: 2/span 4;
+  grid-row: 9/span 8;
+  width: 434px;
+  margin-left: 4rem;
+`
+
+const Article = styled.div`
+  grid-column: 7 / span 4;
+  grid-row: 10 / span 8;
+`
+
+const Paragraphs = styled.p`
+  line-height: 2rem;
+  font-size: 20px;
+  margin: 2rem 0;
+`
 
 export default Peace;
